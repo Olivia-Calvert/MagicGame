b0c253dea82929a441d64f9d42493a4f396c199b
Jennifer Field <43635934+JennyBeanTheSkelepun@users.noreply.github.com>
2018-11-11 15:48:23 +0000

    Started Work on Player Controls
    Set up basic WASD movement, needs some improvement still and needs mesh setting up.

---
+++ Source/MagicGame/PlayerBase.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerBase.h"
+
+
+// Sets default values
+APlayerBase::APlayerBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	TemporaryMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Temp. Mesh"));
+}
+
+// Called when the game starts or when spawned
+void APlayerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!CurrentVelocity.IsZero())
+	{
+		FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+		SetActorLocation(NewLocation);
+	}
+
+}
+
+// Called to bind functionality to input
+void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerBase::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerBase::MoveRight);
+
+}
+
+
+void APlayerBase::MoveForward(float value)
+{
+	CurrentVelocity.X = value * moveSpeed;
+}
+
+void APlayerBase::MoveRight(float value)
+{
+	CurrentVelocity.Y = value * moveSpeed;
+}
+
+
---
+++ Source/MagicGame/PlayerBase.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerBase.generated.h"
+
+UCLASS()
+class MAGICGAME_API APlayerBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerBase();
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* TemporaryMesh;
+
+	UPROPERTY(VisibleAnywhere)
+		FVector CurrentVelocity;
+	UPROPERTY(EditAnywhere)
+		FVector StartPosition;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float moveSpeed = 20000;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void MoveForward(float value);
+	virtual void MoveRight(float value);
+	
+};
